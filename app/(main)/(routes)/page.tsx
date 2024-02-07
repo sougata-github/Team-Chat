b5ee807f558e3a98b76a9aad76ae18b5b76d336f
Sougata <sougatadas9874@gmail.com>
2024-02-07 19:40:32 +0530

    add authentication using clerk.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className="p-20 flex items-center justify-center">
-      <h1 className="font-medium">Discord</h1>
+    <div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
