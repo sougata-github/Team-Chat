8bf0f425e87de484369dad3dc488669392047a3b
Sougata <sougatadas9874@gmail.com>
2024-02-12 19:21:18 +0530

    feat: create server.

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import axios from "axios";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -25,6 +27,7 @@ import { Button } from "../ui/button";
 import FileUpload from "../FileUpload";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -37,6 +40,8 @@ const formSchema = z.object({
 
 const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -53,7 +58,14 @@ const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers/", values);
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
