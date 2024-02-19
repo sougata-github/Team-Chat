6df6e85b5c9a03cac7432e365ba71891133df1d2
Sougata <sougatadas9874@gmail.com>
2024-02-19 22:01:46 +0530

    feat: create a channel.

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -71,7 +71,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("createChannel")}
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
