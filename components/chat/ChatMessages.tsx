a9f8bc1fe669ceffb8ba5db95f497455b90b1443
Sougata <sougatadas9874@gmail.com>
2024-03-16 20:54:21 +0530

    fix typo.

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -78,7 +78,7 @@ const ChatMessages = ({
   if (status === "pending") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Loader2 className="h-7 w-7 text-ainc-500 animate-spin my-4" />
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
         <p className="text-xs text-zinc-500 dark:text-zinc-400">
           Loading messages...
         </p>
